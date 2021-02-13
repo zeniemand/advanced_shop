e19c61df40c0fd05ed3f07da4bd55482ff549987
zeniemand <serhiy_niemand@yahoo.com>
2021-02-13 14:01:10 +0200

    next push

--- config/db_params.php
+++ config/db_params.php
@@ -1,8 +1,19 @@
 <?php
 
 return [
+    'host' => 'eu-cdbr-west-03.cleardb.net',
+    'dbname' => 'heroku_1f5cac5d84273ec',
+    'user' => 'b713f6feab1f49',
+    'password' => 'e2ea6381'
+];
+
+/*
+ *
+return [
     'host' => 'localhost',
     'dbname' => 'adv_shop',
     'user' => 'root',
     'password' => 'admin'
 ];
+
+*/
