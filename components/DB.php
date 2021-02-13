52427e812333672293b42fc26ad10b7005050366
zeniemand <serhiy_niemand@yahoo.com>
2021-02-13 15:30:30 +0200

    next push

--- components/DB.php
+++ components/DB.php
@@ -16,8 +16,36 @@ class DB
         $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
         $db = new PDO($dsn, $params['user'], $params['password']);
 
+        $sql = "ALTER DATABASE `{$params['dbname']}` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+        $res = $db->query($sql);
+        while($row = $res->fetch()){
+            $sql = "ALTER TABLE {$params['dbname']}.`{$row[0]}` CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci";
+            $db->query($sql);
+            $sql = "ALTER TABLE {$params['dbname']}.`{$row[0]}` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+            $db->query($sql);
+
+        }
+
         return $db;
     }
 
 
 }
+
+/*
+
+$dbname = 'my_databaseName';
+mysql_connect('127.0.0.1', 'root', '');
+mysql_query("ALTER DATABASE `$dbname` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+$res = mysql_query("SHOW TABLES FROM `$dbname`");
+while($row = mysql_fetch_row($res)) {
+   $query = "ALTER TABLE {$dbname}.`{$row[0]}` CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci";
+   mysql_query($query);
+   $query = "ALTER TABLE {$dbname}.`{$row[0]}` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+   mysql_query($query);
+}
+echo 'all tables converted';
+
+
+
+ */
